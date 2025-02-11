5f12a82f1175e8d45a93be12837a8a4e990ce99a
Kirito <efraimcanilang404@gmail.com>
2025-02-11 20:45:32 +0800

    Implemented history menus

--- lib/screens/main_screen/main_screen.dart
+++ lib/screens/main_screen/main_screen.dart
@@ -32,6 +32,8 @@ class _MainScreenState extends State<MainScreen> {
   //   }
   // }
 
+  bool showDim = false;
+
   Duration getRemainingTime(String time) {
     final targetDate = DateTime.parse(time);
     final currentDate = DateTime.now();
@@ -110,6 +112,172 @@ class _MainScreenState extends State<MainScreen> {
     );
   }
 
+  Widget buildMenuItem(String text, Function() onClick) {
+    return SizedBox(
+      height: 30, // Set height of the TextButton
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            showDim = false;
+          });
+        },
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.zero, // Remove any default padding
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 14), // Optional: style for text
+        ),
+      ),
+    );
+  }
+
+  Widget buildBalance(String balance) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Column(
+          mainAxisAlignment:
+              MainAxisAlignment.start, // Align children vertically at the start
+          crossAxisAlignment: CrossAxisAlignment
+              .start, // Align children horizontally at the start
+          children: [
+            GestureDetector(
+              onTap: () {
+                // Handle your tap action here
+                setState(() {
+                  showDim = !showDim;
+                });
+              },
+              child: Container(
+                height: 40,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  gradient: const LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color(0xFF282665),
+                      Color(0xff3D3A9A),
+                    ],
+                  ),
+                  border: Border.all(color: Colors.white, width: 1),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: IntrinsicWidth(
+                    child: Center(
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min, // Prevents extra space
+                        children: [
+                          Text(
+                            '₪ $balance ',
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                          const Text(
+                            'יתרה',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 4),
+            if (showDim)
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(7),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    crossAxisAlignment:
+                        CrossAxisAlignment.end, // Align text to the end (right)
+                    children: [
+                      buildMenuItem("היסטורית משיכות מיתרה", () {}),
+                      buildMenuItem("היסטורית ההפקדות", () {}),
+                      buildMenuItem("היסטורית שליחת טפסים", () {})
+                    ],
+                  ),
+                ),
+              ),
+          ],
+        ));
+  }
+
+  Widget buildContents(MainScreenState state, Size screenSize) {
+    return Column(
+      children: [
+        // Header Row
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            children: [
+              const Spacer(), // Use Spacer to push icon to the right
+              IconButton(
+                icon: Image.asset(
+                  '${Constants.imagePath}/profile_icon.png',
+                  height: 40,
+                  width: 40,
+                ),
+                onPressed: () {
+                  context.navigate(Routes.profileNavigation);
+                },
+              ),
+            ],
+          ),
+        ),
+
+        const SizedBox(height: 20), // Fixed spacing
+
+        // Logo
+        Image.asset(
+          '${Constants.imagePath}/main_logo.png',
+          width: screenSize.width * 0.7, // Responsive width
+          height: screenSize.height * 0.13, // Responsive height
+        ),
+
+        const SizedBox(height: 20), // Spacing for countdown
+
+        buildGameCards(state),
+
+        const SizedBox(height: 20), // Spacing for countdown
+
+        ElevatedButton.icon(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color(0xff388B45),
+            foregroundColor: Colors.white, // צבע הטקסט
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8), // פינות מעוגלות
+              side: const BorderSide(color: Colors.white, width: 2), // גבול לבן
+            ),
+          ),
+          onPressed: () {
+            // openWhatsApp('09974663117');
+          },
+          iconAlignment: IconAlignment.end,
+          label: const Text(' צור קשר עם שירות הלקוחות'),
+          icon: Image.asset(
+            '${Constants.imagePath}/wechat_logo.png',
+            width: 25,
+            height: 25,
+          ),
+        ),
+
+        const SizedBox(height: 20), // Spacing for countdown
+      ],
+    );
+  }
+
   @override
   void initState() {
     super.initState();
@@ -129,115 +297,29 @@ class _MainScreenState extends State<MainScreen> {
       body: Stack(
         children: [
           const BackgroundImageScreen(),
+          // if (showDim)
+          //   Container(
+          //     height: double.infinity,
+          //     width: double.infinity,
+          //     color: Colors.black.withAlpha(70),
+          //   ),
           SafeArea(
-              child: BlocConsumer<MainScreenBloc, MainScreenState>(
-                  builder: (context, state) {
-                    final balance = state.response?.balance.toCurrency();
-                    return Column(
-                      children: [
-                        const SizedBox(height: 20), // Fixed spacing
-                        // Header Row
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20),
-                          child: Row(
-                            children: [
-                              Container(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 10),
-                                  height: 40,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(5),
-                                    gradient: const LinearGradient(
-                                      begin: Alignment.topCenter,
-                                      end: Alignment.bottomCenter,
-                                      colors: [
-                                        Color(0xFF282665),
-                                        Color(0xff3D3A9A)
-                                      ],
-                                    ),
-                                    border: Border.all(
-                                        color: Colors.white, width: 1),
-                                  ),
-                                  child: Center(
-                                      child: Row(
-                                    children: [
-                                      Text(
-                                        '₪ $balance ',
-                                        style: const TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 16,
-                                        ),
-                                      ),
-                                      const Text(
-                                        'יתרה',
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 16,
-                                        ),
-                                      )
-                                    ],
-                                  ))),
-                              const Spacer(), // Use Spacer to push icon to the right
-                              IconButton(
-                                icon: Image.asset(
-                                  '${Constants.imagePath}/profile_icon.png',
-                                  height: 40,
-                                  width: 40,
-                                ),
-                                onPressed: () {
-                                  context.navigate(Routes.profileNavigation);
-                                },
-                              ),
-                            ],
-                          ),
-                        ),
-
-                        const SizedBox(height: 20), // Fixed spacing
-
-                        // Logo
-                        Image.asset(
-                          '${Constants.imagePath}/main_logo.png',
-                          width: screenSize.width * 0.7, // Responsive width
-                          height: screenSize.height * 0.13, // Responsive height
-                        ),
-
-                        const SizedBox(height: 20), // Spacing for countdown
-
-                        buildGameCards(state),
-
-                        const SizedBox(height: 20), // Spacing for countdown
-
-                        ElevatedButton.icon(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color(0xff388B45),
-                            foregroundColor: Colors.white, // צבע הטקסט
-                            shape: RoundedRectangleBorder(
-                              borderRadius:
-                                  BorderRadius.circular(8), // פינות מעוגלות
-                              side: const BorderSide(
-                                  color: Colors.white, width: 2), // גבול לבן
-                            ),
-                          ),
-                          onPressed: () {
-                            // openWhatsApp('09974663117');
-                          },
-                          iconAlignment: IconAlignment.end,
-                          label: const Text(' צור קשר עם שירות הלקוחות'),
-                          icon: Image.asset(
-                            '${Constants.imagePath}/wechat_logo.png',
-                            width: 25,
-                            height: 25,
-                          ),
-                        ),
-
-                        const SizedBox(height: 20), // Spacing for countdown
-                      ],
-                    );
-                  },
-                  listener: (context, state) => {
-                        if (state.isLoading == false)
-                          {loadingBloc.add(LoadingEventHide())},
-                      })),
+            child: BlocConsumer<MainScreenBloc, MainScreenState>(
+              builder: (context, state) {
+                final balance = state.response?.balance.toCurrency();
+                return Stack(
+                  children: [
+                    buildContents(state, screenSize),
+                    buildBalance('$balance'),
+                  ],
+                );
+              },
+              listener: (context, state) => {
+                if (state.isLoading == false)
+                  {loadingBloc.add(LoadingEventHide())},
+              },
+            ),
+          ),
         ],
       ),
     );
